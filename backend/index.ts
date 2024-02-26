4415afe2149ce37b623c899deb666769c2fbd784
Sigill2003 <utma22030633@utma.edu.mx>
2024-02-26 13:22:43 -0600

    Cambios del Proyecto para sertificarnos

--- backend/index.ts
+++ backend/index.ts
@@ -2,6 +2,26 @@ import { Server, ic } from 'azle';
 import cors from "cors";
 import express from 'express';
 
+type Tevents = {
+    id:number
+    start_time:string;
+    end_time:string;
+    name:string;
+    place:string;
+}
+
+
+
+const event: Tevents[]=[{
+    id:1234,
+    start_time:"4:00 pm",
+    end_time:"6:30 pm",
+    name:"Comida",
+    place:"Cancha"
+}
+]
+
+
 export default Server(() => {
     const app = express();
 
@@ -16,6 +36,24 @@ export default Server(() => {
     //         next();
     //     }
     // });
+
+    app.post('/create', (req, res) => {
+        const  event = events.find((event)=>event.id == req.body.id);
+        if (event){
+            res.status(400).json({msg:"El id ya esta en uso", data:event});
+            return;
+        }
+        events = [...events,req.body]
+        res.status(200).json({msg:"Evento añadido"});
+    });
+
+    app.get('/get',(req,res)=>{
+        res.status(200).json({msg:"Eventos obtenidos con exito", data:events});
+    });
+
+    app.put('/update/:id')
+
+    
 
     app.post('/test', (req, res) => {
         res.json(req.body);
