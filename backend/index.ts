5cc908adfeb1b8a0b03587ac73189257f465f3db
Sigill2003 <utm22030633@utma.edu.mx>
2024-02-27 10:42:58 -0600

    Cambios del Proyecto para certificacion

--- backend/index.ts
+++ backend/index.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import express from 'express';
 
 type Tevents = {
-    id:number
+    id:number;
     start_time:string;
     end_time:string;
     name:string;
@@ -11,16 +11,13 @@ type Tevents = {
 }
 
 
-
-const event: Tevents[]=[{
+let events: Tevents[] = [{
     id:1234,
     start_time:"4:00 pm",
     end_time:"6:30 pm",
     name:"Comida",
     place:"Cancha"
-}
-]
-
+}];
 
 export default Server(() => {
     const app = express();
@@ -28,44 +25,34 @@ export default Server(() => {
     app.use(cors());
     app.use(express.json());
 
-    // app.use((req, res, next) => {
-    //     if (ic.caller().isAnonymous()) {
-    //         res.status(401);
-    //         res.send();
-    //     } else {
-    //         next();
-    //     }
-    // });
-
     app.post('/create', (req, res) => {
-        const  event = events.find((event)=>event.id == req.body.id);
-        if (event){
-            res.status(400).json({msg:"El id ya esta en uso", data:event});
+        const existingEvent = events.find(event => event.id === req.body.id);
+        if (existingEvent) {
+            res.status(400).json({ msg: "El id ya está en uso", data: existingEvent });
             return;
         }
-        events = [...events,req.body]
-        res.status(200).json({msg:"Evento añadido"});
+        events.push(req.body);res.status(200).json({ msg: "Evento añadido" });
+    });
+
+    app.delete('/delete/:id', (req,res)=> {
+        events = events.filter((e) => e.id !== parseInt(req.params.id));
+        res.status(200).json({msg:"Evento eliminado"});
+    });
+
+    app.put('/update/:id', (req, res)=>{
+        const event = events.find((event) => event.id === parseInt(req.params.id));
+        if(!event){
+            res.status(404).json({msg:"Evento no encontrado"});
+            return;
+        }
+
+        const updatedEvent = {...event, ...req.body};
+        events = events.map((e) => e.id === updatedEvent.id ? updatedEvent : e);
+        res.status(200).json({msg:"El evento se ha actualizado "});
     });
 
     app.get('/get',(req,res)=>{
-        res.status(200).json({msg:"Eventos obtenidos con exito", data:events});
-    });
-
-    app.put('/update/:id')
-
-    
-
-    app.post('/test', (req, res) => {
-        res.json(req.body);
-    });
-
-    app.get('/whoami', (req, res) => {
-        res.statusCode = 200;
-        res.send(ic.caller());
-    });
-
-    app.get('/health', (req, res) => {
-        res.send().statusCode = 204;
+        res.status(200).json({msg:"Eventos obtenidos con éxito", data:events});
     });
 
     return app.listen();
